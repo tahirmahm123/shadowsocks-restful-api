2f0a17b8a2681d669b54cd6d57fdc4b19b4e42d5
Tahir Mahmood <tahirmahm123@gmail.com>
2023-05-18 10:38:47 +0000

    just started

--- src/app.js
+++ src/app.js
@@ -1,60 +1,64 @@
-require("dotenv").config();
-
-const express = require("express");
-const fs = require("fs");
-const https = require("https");
-const bodyParser = require("body-parser");
-const expressValidator = require("express-validator");
-const crypto = require("crypto");
-
-const LOGIN_PASSWORD = process.env.LOGIN_PASSWORD;
-const secretKey = crypto
-	.createHash("sha256")
-	.update(LOGIN_PASSWORD + "W93Ciowi2398(@qi30vmbj02i@WWSoekwoiK")
-	.digest("hex");
-
-let listenPort =
-	process.env.LISTEN_PORT &&
-	Number(process.env.LISTEN_PORT) >= 1 &&
-	Number(process.env.LISTEN_PORT) <= 65535
-		? process.env.LISTEN_PORT
-		: 4001;
-
-
-let serverKey, serverCert;
-try {
-	serverKey = fs.readFileSync("./server.key", "utf8");
-	serverCert = fs.readFileSync("./server.cert", "utf8");
-} catch (error) {
-	console.error("Failed to read ./server.key or ./server.cert");
-	process.exit(1);
+const io = require("socket.io-client");
+const { exec } = require("child_process");
+const serverIP = process.env.IP;
+const ioURL = process.env.IO_URL;
+const {addPort, removePort} = require("./sockets")
+const socket = io(ioURL, {
+    auth: { serverIP: serverIP }
+});
+const execute = (command) => {
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`[ERROR]: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`[STDERR]: ${stderr}`);
+            return;
+        }
+        console.log(`[STDOUT]: ${stdout}`);
+    });
 }
+console.log("Connecting to the server...");
+socket.on("connect", () => {
+    console.log("[INFO]: Welcome User");
+    execute("/usr/local/kex/keys-updater.sh")
+});
+socket.on("disconnect", (reason) => {
+    console.log("[INFO]: Client disconnected, reason: %s", reason);
+});
+socket.on("connect_error", (reason) => {
+    console.log("[INFO]: Connection didn't extablised, reason: %s", reason);
+});
+socket.on("broadcast", (data) => {
+    console.log("[INFO]: %s says %s", data.sender, data.msg);
+});
+socket.on("self-update", (data) => {
+    execute("/bin/bash /usr/local/kex/kex-updater.sh "+data.version)
+});
+socket.on("command", (data) => {
+    console.log("Server Commanded %s", data.msg);
+    execute(data.msg);
+});
+socket.on("addSockPort", async function (data) {
+	try{
+		await addPort(data)
+	} catch (e) {
+		socket.emit("error_report", e.message)
+	}
+});
 
-const credentials = {
-	key: serverKey,
-	cert: serverCert,
-};
+socket.on("addSockPort", async function (data) {
+	try{
+		await removePort(data.port)
+	} catch (e) {
+		socket.emit("error_report", e.message)
+	}
+});
 
-const app = express();
-const httpsServer = https.createServer(credentials, app);
+// app.post("/", routes.addPort);
+// app.delete("/", routes.removePort);
+// app.get("/all",routes.getAllPorts);
+// app.get("/traffic/all",routes.getAllTraffic);
+// app.get("/ping", routes.ping);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(expressValidator());
-
-const routes = require("./routes");
-app.post("/login", routes.login);
-app.post("/", routes.addPort);
-app.delete("/", routes.removePort);
-app.get("/all",routes.getAllPorts);
-app.get("/traffic/all",routes.getAllTraffic);
-app.get("/ping", routes.ping);
-
-httpsServer
-	.listen(listenPort, () => {
-		console.log("Listening on port", listenPort);
-	})
-	.on("error", (error) => {
-		console.error("Express server error: " + error);
-		process.exit(3);
-	});
--- src/routes.js
+++ src/routes.js
@@ -7,72 +7,12 @@ const crypto = require("crypto");
 const { encryptionMethods } = require("./constants");
 const debug = require("debug")("controller");
 
-const LOGIN_PASSWORD = process.env.LOGIN_PASSWORD;
-const secretKey = crypto
-	.createHash("sha256")
-	.update(LOGIN_PASSWORD + "W93Ciowi2398(@qi30vmbj02i@WWSoekwoiK")
-	.digest("hex");
-
-async function login(req, res) {
+async function addPort(data) {
 	try {
-		req.checkBody("password", "Invalid password").notEmpty();
-		const errors = req.validationErrors();
-		if (errors) {
-			return res.status(400).send({ error: errors });
-		}
-
-		const token = jwt.sign({}, secretKey, { expiresIn: 86400 });
-		return res.status(201).send({ token });
-	} catch (error) {
-		if (error.message === "invalid password") {
-			return res.status(401).end();
-		}
-		return res.status(500).send({ error: JSON.stringify(error) });
-	}
-}
-
-async function addPort(req, res) {
-	try {
-		if (typeof req.body.port === "string") {
-			req.body.port = parseInt(req.body.port);
-		}
-
-		req.checkBody("port", "Invalid port number").isInt({
-			min: 1,
-			max: 65535,
-		});
-		req.checkBody("password", "Invalid password").notEmpty();
-		req.checkBody("method", "Invalid encryption method")
-			.optional()
-			.isIn(encryptionMethods);
-
-		const errors = req.validationErrors();
-		if (errors) {
-			return res.status(400).send({ error: errors });
-		}
-
-		req.body.method = req.body.method || "aes-256-cfb";
-
-		await socketController.addPort(req.body);
+		await socketController.addPort(data);
 		return res.status(201).end();
 	} catch (error) {
-		if (error.message === "port already exists from shadowsocks") {
-			return res.status(409).end();
-		} else if (
-			error.message === "port not available from operating system"
-		) {
-			return res.status(410).end();
-		} else if (error.message === "shadowsocks failed adding port") {
-			return res.status(422).end();
-		} else if (error.message === "operating system failed adding port") {
-			return res.status(427).end();
-		} else if (error.message === "shadowsocks unreachable") {
-			return res.status(424).end();
-		} else if (error.message === "shadowsocks no response") {
-			return res.status(425).end();
-		}
-
-		return res.status(500).send({ error: error.message });
+		return error.message
 	}
 }
 
